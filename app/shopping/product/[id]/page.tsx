272ebc0886c76f9389b4c5aae25d0d576e37674c
Carlos Gomes <carlos_gomes1809@hotmail.com>
2023-08-06 01:30:26 -0300

    add some other typings for products and initial structure for product page

---
+++ app/shopping/product/[id]/page.tsx
@@ -0,0 +1,50 @@
+import { getFetchUrl } from "@/lib/getFetchUrl";
+import { ProductData } from "@/typings";
+import { StarIcon } from "@heroicons/react/24/solid";
+import { notFound } from "next/navigation";
+
+export const revalidate = 300;
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+async function ProductPage({ params: { id } }: Props) {
+  const response = await fetch(getFetchUrl(`api/shopping/product/${id}`));
+  const productData = (await response.json()) as ProductData;
+
+  if (!productData.content.pricing) {
+    return notFound();
+  }
+
+  return (
+    <div className="p-12 pt-0">
+      <h1 className="text-2xl">{productData.content.title}</h1>
+
+      {productData.content.reviews && (
+        <div className="flex space-x-1">
+          {[
+            ...Array.from({
+              length: Math.round(productData.content.reviews.rating),
+            }),
+          ].map((_, i) => (
+            <StarIcon key={i} className="w-5 h-5 text-yellow-500" />
+          ))}
+
+          {/* show remaining stars from review out of 5 */}
+          {[
+            ...Array.from({
+              length: 5 - Math.round(productData.content.reviews.rating),
+            }),
+          ].map((_, i) => (
+            <StarIcon key={i} className="w-5 h-5 text-gray-200" />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProductPage;
